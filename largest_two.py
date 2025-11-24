46aa8393bc351df59c9569f9590743834dc4e5a4
Kalyani_karri <venkatakalyanikarri913@gmail.com>
2025-11-24 19:13:35 +0530

    Add files via upload

---
+++ largest_two.py
@@ -0,0 +1,6 @@
+num1 = int(input("enter the num1: "))
+num2 = int(input("enter the num2: "))
+if num1>num2:
+    print(f"{num1} is greater than {num2}")
+else:
+    print(f"{num1} is lower than {num2}")
