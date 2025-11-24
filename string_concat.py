131c8001138af22d9f06568d5c4798dc5ecc5260
Kalyani_karri <venkatakalyanikarri913@gmail.com>
2025-11-24 17:37:37 +0530

    Add files via upload

---
+++ string_concat.py
@@ -0,0 +1,16 @@
+s="VEnkata"
+s2="kalyani"
+s3="  xxx kalyani   "
+t_str=s+" "+s2
+print(f"total srting:{t_str}")
+print(s.upper())
+print(s2.lower())
+print(s3.strip("x "))
+print(s3.replace('x','a'))
+print(s3.count('x'))
+"srting formating"
+print("My name is %s %s and my age is %d" %("venkata","kalyani",19))
+"now a days way if fstring s"
+print(f"My name is {"venkata"} {"kalyani"} and my age is {19}")
+
+
